c845ff176b6fc3473eeaab33f5cd116e650dbb0a
CharlesJV <charles764jaison@gmail.com>
2020-06-01 17:25:42 -0400

    added Admin Panel for new products

--- repositories/users.js
+++ repositories/users.js
@@ -1,110 +1,43 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const util = require('util');
+const Repository = require('./repository');
 
 const scrypt = util.promisify(crypto.scrypt);
 
 
-class UsersRepository{
+class UsersRepository extends Repository{
 
-constructor(filename){
-
-if(!filename){
-    throw new Error('Creating a repository requires a filename');
-}
-
-this.filename = filename;
-try{
-    fs.accessSync(this.filename);
-}
-catch(err){
-    fs.writeFileSync(this.filename,'[]');
-}
-}
-
-async getAll(){
-
-    return JSON.parse(await fs.promises.readFile(this.filename,{encoding: 'utf-8'}))
-}
-
-async getOne(id){
-    const records = await this.getAll();
-    return records.find(record => record.id === id );
-}
-
-async create(attrs){
-    attrs.id = this.randomId();
-
-    const salt = crypto.randomBytes(8).toString('hex');
-    const buf = await scrypt(attrs.password,salt, 64);
-
-
-    const records = await this.getAll();
-   
-   const record = ({
-        ...attrs,
-        password: `${buf.toString('hex')}.${salt}`
-    });
-
-    records.push(record);
-    await this.writeAll(records)
-
-    return record;
-}
-
-async comparePasswords(supplied,saved){
-
-    const[hashed,salt] = saved.split('.');
-
-    const hashedSuppliedBuf = await scrypt(supplied,salt, 64);
-
-    return hashed === hashedSuppliedBuf.toString('hex');
-
-}
-
-
-async writeAll(records){
-    await fs.promises.writeFile(this.filename,JSON.stringify(records,null,2));
-}
-
-async delete(id){
-    const records = await this.getAll();
-    const filteredRecords = records.filter(record => record.id !== id);
-    await this.writeAll(filteredRecords);
-}
-
-async update(id, attrs){
-    const records = await this.getAll();
-    const  record = records.find(record => record.id === id);
-
-    if(!record){
-        throw new Error(`Record with an id of ${id} is not found to overwrite`);
+    async comparePasswords(supplied,saved){
+        
+        const[hashed,salt] = saved.split('.');
+    
+        const hashedSuppliedBuf = await scrypt(supplied,salt, 64);
+    
+        return hashed === hashedSuppliedBuf.toString('hex');
+    
     }
+    
 
-    Object.assign(record, attrs);
-    await this.writeAll(records);
-
-}
-
-async getOneBy(filter){
-    const records = await this.getAll();
-    for(let record of records){
-        let found = true;
-
-        for(let key in filter){
-            if(record[key] !== filter[key]){
-                found = false;
-            }
-        }
-        if(found){
-            return record;
-        }
+    async create(attrs){
+        attrs.id = this.randomId();
+    
+        const salt = crypto.randomBytes(8).toString('hex');
+        const buf = await scrypt(attrs.password,salt, 64);
+    
+    
+        const records = await this.getAll();
+       
+       const record = ({
+            ...attrs,
+            password: `${buf.toString('hex')}.${salt}`
+        });
+    
+        records.push(record);
+        await this.writeAll(records)
+    
+        return record;
     }
-}
-
-randomId(){
-    return crypto.randomBytes(4).toString('hex');
-}
 
 }
 
