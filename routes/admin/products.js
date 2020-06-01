3a4413fd191cefacee1dd78a8711a31c3dc74de8
CharlesJV <charles764jaison@gmail.com>
2020-06-01 17:45:16 -0400

    Added validation for price and title of products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const {validationResult} = require('express-validator');
 const productsRepo = require('../../repositories/products');
-const productsNewTemplate = require('../../views/admin/products/new'); 
+const productsNewTemplate = require('../../views/admin/products/new');
+const {requireTitle,requirePrice} = require('./validator'); 
 
 router.get('/admin/products',(req,res)=>{
 
@@ -9,6 +11,13 @@ router.get('/admin/products',(req,res)=>{
 
 router.get('/admin/products/new',(req,res)=>{
     res.send(productsNewTemplate({}));
+
+})
+
+router.post('/admin/products/new',[requireTitle,requirePrice],(req,res)=>{
+    const errors = validationResult(req);
+    console.log(errors);
+    res.send('submitted');
 
 })
 
