912c9f033db230b370f6bf14a9ff9552cdaa1a4a
CharlesJV <charles764jaison@gmail.com>
2020-06-01 16:32:07 -0400

    products router using express.js

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const bodyParser = require('body-parser');
 const usersRepo = require('./repositories/users');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-
+const productsRouter = require('./routes/admin/products');
 
 
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
     keys: ['tyuilmnfldsgjldmvld']
 }))
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3010,()=>{
     console.log('Listening');
