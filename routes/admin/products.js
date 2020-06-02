a86959454903f1c0fac323745e6f5c31da80c223
CharlesJV <charles764jaison@gmail.com>
2020-06-02 06:55:19 -0400

    Added multer library

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const router = express.Router();
+
 const {validationResult} = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const {requireTitle,requirePrice} = require('./validator'); 
+
+const router = express.Router();
+const upload = multer({storage: multer.memoryStorage()});
 
 router.get('/admin/products',(req,res)=>{
 
@@ -14,10 +19,23 @@ router.get('/admin/products/new',(req,res)=>{
 
 })
 
-router.post('/admin/products/new',[requireTitle,requirePrice],(req,res)=>{
+router.post('/admin/products/new',
+upload.single('image'),
+[requireTitle,requirePrice],
+async(req,res)=>{
+
     const errors = validationResult(req);
-    console.log(errors);
-    res.send('submitted');
+
+    if(!errors.isEmpty()){
+        res.send(productsNewTemplate({errors}));
+    }
+
+   const image = req.file.buffer.toString('base64');
+   const{title,price} = req.body;
+
+    await productsRepo.create({title,price,image});
+
+     res.send('submitted');
 
 })
 
--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -1,13 +1,16 @@
 const layout = require('../layout');
 const helpers = require('../../helper');
+const {getErrors} = require('../../helper');
 
 module.exports = ({errors})=>{
 
 return layout({
 content: `
-<form method = "POST">
+<form method = "POST" enctype="multipart/form-data" >
 <input placeholder = "Title" name = "title"/>
+${getErrors(errors,'title')}
 <input placeholder = "Price" name = "price">
+${getErrors(errors,'price')}
 <input type = "file" name = "image"/>
 <button>Submit</button>
 </form>
