58aea399f72e83873c3eec1b43dd76fd54711483
CharlesJV <charles764jaison@gmail.com>
2020-06-03 16:50:46 -0400

    Added a form element to the cart button to hold the product id

--- routes/carts.js
+++ routes/carts.js
@@ -2,5 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+//Receive a post request to add an item to the cart
+
+
+// Receive a get request to show all items in the cart
+
+// Receive a post request to delete an item from  the cart
 
 module.exports = router;
