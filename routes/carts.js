c2aca17adf5e8ece3be35b92c226d01708f269c5
CharlesJV <charles764jaison@gmail.com>
2020-06-04 06:24:45 -0400

    Added styling to the cart page

--- routes/carts.js
+++ routes/carts.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const cartsRepo = require('../repositories/carts')
-
+const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 const router = express.Router();
 
 //Receive a post request to add an item to the cart
@@ -37,6 +38,20 @@ const router = express.Router();
 
 // Receive a get request to show all items in the cart
 
+router.get('/cart',async(req,res)=>{
+    if(!req.session.cartId){
+        return res.redirect('/');
+    }
+
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    for(let item of cart.items){
+        const product = await productsRepo.getOne(item.id);
+        item.product = product;
+    }
+    console.log(cart.items);
+    res.send(cartShowTemplate({items: cart.items}))
+})
 // Receive a post request to delete an item from  the cart
 
 module.exports = router;
